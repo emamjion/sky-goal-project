40b1df0f2db5005e8819cc4bb5f4e73c11487346
emamjion <emamjion@gmail.com>
2023-11-25 15:03:38 +0600

    react router setup finished

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Main from './Layout/Main'
+import Home from './Pages/Home/Home/Home'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main/>,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      }
+    ]
+  }
+])
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
